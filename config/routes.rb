0e97c4b542df43f4364dd1d2500c78f0b09b9158
EC2 Default User <ec2-user@ip-172-31-3-6.ap-northeast-1.compute.internal>
2023-07-19 02:32:52 +0000

    Initial Commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,20 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :public do
+    resources :posts, only: [:new, :index, :show, :edit]
+  end
+  namespace :public do
+    gresources :topics, only: [:index, :show]
+  end
+  devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+  sessions: "admin/sessions"
+}
+  devise_for :customers,skip: [:passwords], controllers: {
+  registrations: "public/registrations",
+  sessions: 'public/sessions'
+}
+root to: "homes#top"
+get '/homes/about' => "homes#about",as: 'about'
+  resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+    resources :post_comments, only: [:create]
+  end
 end
